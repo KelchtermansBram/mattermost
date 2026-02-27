ecaf67732debc1c3c15491e477d17c3b291b7d8e
Bram <info@bramkelchtermans.be>
2026-02-27 14:59:54 +0100

    would this work?

--- webapp/channels/src/components/global_header/left_controls/product_menu/product_branding_team_edition/product_branding_free_edition.tsx
+++ webapp/channels/src/components/global_header/left_controls/product_menu/product_branding_team_edition/product_branding_free_edition.tsx
@@ -1,15 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {useSelector} from 'react-redux';
-import styled from 'styled-components';
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
 
-import {getLicense} from 'mattermost-redux/selectors/entities/general';
+import { getLicense } from "mattermost-redux/selectors/entities/general";
 
-import Logo from 'components/common/svg_images_components/logo_dark_blue_svg';
+import Logo from "components/common/svg_images_components/logo_dark_blue_svg";
 
-import {LicenseSkus} from 'utils/constants';
+import { LicenseSkus } from "utils/constants";
 
 const ProductBrandingFreeEditionContainer = styled.span`
     display: flex;
@@ -34,7 +34,7 @@ const Badge = styled.span`
     margin-left: 12px;
     background: rgba(var(--sidebar-text-rgb), 0.08);
     color: rgba(var(--sidebar-text-rgb), 0.75);
-    font-family: 'Open Sans', sans-serif;
+    font-family: "Open Sans", sans-serif;
     font-size: 10px;
     font-weight: 600;
     letter-spacing: 0.025em;
@@ -44,16 +44,18 @@ const Badge = styled.span`
 const ProductBrandingFreeEdition = (): JSX.Element => {
     const license = useSelector(getLicense);
 
-    let badgeText = '';
+    let badgeText = "";
     if (license?.SkuShortName === LicenseSkus.Entry) {
-        badgeText = 'ENTRY EDITION';
-    } else if (license?.IsLicensed === 'false') {
-        badgeText = 'TEAM EDITION';
+        badgeText = "ENTRY EDITION";
+    } else if (license?.IsLicensed === "false") {
+        badgeText = "TEAM EDITION";
     }
 
     return (
         <ProductBrandingFreeEditionContainer tabIndex={-1}>
-            <StyledLogo
+            <img
+                src="https://comedykit.be/images/logo.avif"
+                alt="ComedyKit Logo"
                 width={116}
                 height={20}
             />
